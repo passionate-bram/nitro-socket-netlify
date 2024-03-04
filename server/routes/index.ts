5dc37a22e9cc37253199a65307c5756581116906
bramkamies <bramkamies@passionatepeople.io>
2024-03-04 14:50:20 +0100

    Add websocket sample

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,3 +1,5 @@
-export default eventHandler((event) => {
-  return "Start by editing <code>server/routes/index.ts</code>.";
+export default defineEventHandler(() => {
+  return $fetch(
+    "https://raw.githubusercontent.com/unjs/crossws/main/examples/h3/public/index.html"
+  );
 });
